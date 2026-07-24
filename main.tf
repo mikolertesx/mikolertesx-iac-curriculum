b5974b9b861ae326746a7ac07b3fd54c16df4d67
Miguel Angel <mgangsal@outlook.com>
2026-07-23 21:14:56 -0500

    Fix configuration so that website hosting works

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-locks-1447"
-    key    = "global/curriculum/prod"
-    region = "us-east-1"
+    bucket       = "terraform-locks-1447"
+    key          = "global/curriculum/prod"
+    region       = "us-east-1"
     use_lockfile = true
   }
 }
@@ -18,10 +18,10 @@ locals {
 
 resource "aws_s3_bucket" "website_host_bucket" {
   bucket_namespace = "global"
-  bucket = local.s3_bucket_name
+  bucket           = local.s3_bucket_name
 
   tags = {
-    Name = "website host bucket"
+    Name        = "website host bucket"
     Environment = "Prod"
   }
 }
@@ -38,26 +38,65 @@ resource "aws_s3_bucket_website_configuration" "website_static_config" {
   }
 }
 
-resource "aws_s3_object" "indexFile" {
+# Remove this once we have CloudFront
+resource "aws_s3_bucket_public_access_block" "remove_public_access_block" {
   bucket = local.s3_bucket_name
-  key = "index.html"
-  source = "./src/index.html"
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_policy" "apply_allow_public_access_policy" {
+  bucket = local.s3_bucket_name
+  policy = data.aws_iam_policy_document.allow_public_access_policy.json
+
+}
+
+resource "aws_s3_object" "indexFile" {
+  bucket       = local.s3_bucket_name
+  key          = "index.html"
+  source       = "./src/index.html"
+  content_type = "text/html"
 }
 
 resource "aws_s3_object" "stylesFile" {
-  bucket = local.s3_bucket_name
-  key = "styles.css"
-  source = "./src/styles.css"
+  bucket       = local.s3_bucket_name
+  key          = "styles.css"
+  source       = "./src/styles.css"
+  content_type = "text/css"
 }
 
 resource "aws_s3_object" "imageFile" {
-  bucket = local.s3_bucket_name
-  key = "portrait.jpeg"
-  source = "./src/portrait.jpeg"
+  bucket       = local.s3_bucket_name
+  key          = "portrait.jpeg"
+  source       = "./src/portrait.jpeg"
+  content_type = "image/jpeg"
 }
 
 resource "aws_s3_object" "jsFile" {
-  bucket = local.s3_bucket_name
-  key = "vistors.js"
-  source = "./src/visitors.js"
+  bucket       = local.s3_bucket_name
+  key          = "vistors.js"
+  source       = "./src/visitors.js"
+  content_type = "text/javascript"
+
+}
+
+data "aws_iam_policy_document" "allow_public_access_policy" {
+  statement {
+    sid    = "PublicReadGetObject"
+    effect = "Allow"
+    actions = [
+      "s3:GetObject"
+    ]
+    principals {
+      type        = "AWS"
+      identifiers = ["*"]
+    }
+    resources = [
+      aws_s3_bucket.website_host_bucket.arn,
+      "${aws_s3_bucket.website_host_bucket.arn}/*"
+    ]
+  }
 }
