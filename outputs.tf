c54b345f609580684fc3f7916cc57c5d18e2bc29
Miguel Angel <mgangsal@outlook.com>
2026-07-24 18:16:57 -0500

    Add outputs to display cloudFront domain, apex domain, and www domain

--- outputs.tf
+++ outputs.tf
@@ -3,7 +3,15 @@ output "cloudFront_domain" {
   description = "CloudFront distribution's domain name"
 }
 
-output "cloudFront_alternative_names" {
-  value = aws_cloudfront_distribution.s3_distribution.aliases
-  description = "CloudFront distribution's aliases"
+output "cloudfront_apex_domain" {
+  value = tolist(aws_cloudfront_distribution.s3_distribution.aliases)[0]
+  description = "CloudFront Apex domain"
+}
+
+output "cloudfront_www_domain" {
+  value = one([
+    for alias in aws_cloudfront_distribution.s3_distribution.aliases : alias
+    if startswith(alias, "www.")
+  ])
+  description = "CloudFront WWW domain"
 }
